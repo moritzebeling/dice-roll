d079fdb38125c7a69fbd7fc8f84d56d238524fb9
moritzebeling <git@moritzebeling.com>
2023-02-23 18:20:53 +0100

    update stuff

--- src/lib/Meta.svelte
+++ src/lib/Meta.svelte
@@ -1,13 +1,26 @@
 <svelte:head>
     
-    <title>Dice Roll</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+
+    <title>Dice</title>
+    <meta name="application-name" content="Dice">
+    <meta name="apple-mobile-web-app-title" content="Dice">
 
     <link rel="icon" href="/favicon.svg" type="image/svg+xml">
     <link rel="icon" href="/favicon.png" type="image/png" />
     <link rel="apple-touch-icon" href="/favicon.png" />
+    <link rel="mask-icon" href="/favicon.svg" color="#000000">
 
     <meta name="description" content="Roll a dice">
     <meta name="keywords" content="dice, roll, rolling, game, play, backgammon">
     <meta name="author" content="Moritz Ebeling">
+
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="mobile-web-app-capable" content="yes">
+
+    <meta name="theme-color" content="#000000">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+
+    <link rel="manifest" href="/manifest.json">
 
 </svelte:head>
